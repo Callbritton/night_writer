aba7246af2f36f1e0f1efc7709420b06d514cf74
Chris Allbritton <cla.inbox@gmail.com>
2020-07-31 09:49:13 -0400

    Updated night_writer with dynamic ability to print file created and character count

--- night_writer.rb
+++ night_writer.rb
@@ -1,10 +1,11 @@
 input_file = ARGV[0]
 output_file = ARGV[1]
 contents = File.read(input_file)
-# now just convert to brail.
+
 File.open(output_file, "w") do |f|
   f.write contents
 end
 
+puts "Created #{output_file} containing #{File.read(output_file).chomp.length} characters"
 #ruby night_writer.rb test.txt (ARGV[0]) log.txt(ARGV[1])
 #text typed in test.txt will be written into log.txt
