a55ba9b61e70a3f6953c81e2301ee7e28bea0c3f
Chris Allbritton <cla.inbox@gmail.com>
2020-07-31 09:55:30 -0400

    Update night_writer in preperation for conversion to class and proper TDD and VC management

--- night_writer.rb
+++ night_writer.rb
@@ -9,3 +9,6 @@ end
 puts "Created #{output_file} containing #{File.read(output_file).chomp.length} characters"
 #ruby night_writer.rb test.txt (ARGV[0]) log.txt(ARGV[1])
 #text typed in test.txt will be written into log.txt
+
+# Working base file to wrap up Iteration 1. This now needs to be refactored into
+# functional class with tests, etc. 
