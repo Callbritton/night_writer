47900256bbddf3a3603807878ae86e54efa6de8e
Chris Allbritton <cla.inbox@gmail.com>
2020-08-01 09:36:25 -0400

    Add test_it_exists to converter_test

---
+++ test/converter_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/dictionary"
+require "./test/test_helper"
+
+class ClassTest < Minitest::Test
+
+  def test_it_exists
+    converter = Converter.new
+    assert_instance_of Converter, converter
+  end
+
+end 
