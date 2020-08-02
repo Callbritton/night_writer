05b338a51f7bc0ca740a786e50dfa4a22e3218ec
Chris Allbritton <cla.inbox@gmail.com>
2020-08-02 13:23:01 -0400

    Add test_it_can_create_sliced_contents test and test_input_file_contents test

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -3,6 +3,7 @@ require "minitest/pride"
 require "./lib/converter"
 require "./lib/night_writer"
 require "./test/test_helper"
+require "mocha/minitest"
 
 class ConverterTest < Minitest::Test
 
@@ -11,13 +12,25 @@ class ConverterTest < Minitest::Test
     assert_instance_of NightWriter, night_writer
   end
 
-  def test_it_can_write_braille_to_file
-    File.write("message.txt", "a")
-    night_writer = NightWriter.new("message.txt", "braille.txt")
-    # night_writer.execute_conversion
-    expected = "0.\n..\n.."
+  # def test_it_can_write_braille_to_file
+  #   File.expects(:readlines).with("message.txt").returns("a")
+  #   night_writer = NightWriter.new("message.txt", "braille.txt")
+  #   # night_writer.execute_conversion
+  #   expected = "0.\n..\n.."
+  #
+  #   assert_equal expected, File.read("braille.txt")
+  # end
 
-    assert_equal expected, File.read("braille.txt")
+  def test_it_can_create_sliced_contents
+    night_writer = NightWriter.new("would_be_input", "would_be_output")
+    contents = ["aeiou"]
+    assert_equal %w(a e i o u), night_writer.create_sliced_contents(contents, 1)
+  end
+
+  def test_input_file_contents
+    File.expects(:readlines).with("message.txt").returns("Testing mocks")
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    assert_equal "Testing mocks", night_writer.input_file_contents
   end
 
 end
