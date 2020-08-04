d66a7c4312037836ceeeb40d986c7155f776807f
Chris Allbritton <cla.inbox@gmail.com>
2020-08-03 21:16:56 -0400

    Added test_it_can_split_letters test_it_can_convert_letters test_it_can_slice_braille to test interdependent methods

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,8 +1,42 @@
+require "./lib/dictionary"
 class NightReader
+  include Dictionary
+  attr_reader :input_file,
+              :output_file
 
   def initialize(input_file, output_file)
-
+    @input_file = input_file
+    @output_file = output_file
   end
 
+  
+  def convert_braille(processed_braille)
+    collected_strings = []
+    processed_braille.each do |braille_char|
+      collected_strings << braille_to_letters[braille_char]
+    end
+    collected_strings = collected_strings.flatten.join
+  end
 
+  def convert_into_letters(prepared_braille)
+    # number of letters divided by
+    braille_to_letters_ratio = 2
+    number_of_letters = prepared_braille.first.length / braille_to_letters_ratio
+    split_braille = []
+    number_of_letters.times do
+      sliced_braille(split_braille, prepared_braille)
+    end
+    # this splits the input array into multiple
+    # arrays of 3 # elements
+    processed_braille = split_braille.each_slice(3).to_a
+    # this converts the multiple arrays into letters!
+    convert_braille(processed_braille)
+  end
+  # pulls off the first 2 characters of each line
+  def sliced_braille(split_braille, prepared_braille)
+    prepared_braille.each do |string|
+      split_braille << string[0..1]
+      string.slice!(0..1)
+    end
+  end
 end
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -6,4 +6,31 @@ class ConverterTest < Minitest::Test
     assert_instance_of NightReader, night_reader
   end
 
+  def test_it_can_convert_into_letters
+    night_reader = NightReader.new("would_be_input", "would_be_output")
+    assert_equal "hello aric this is a big long string for", night_reader.convert_into_letters([
+      "0.0.0.0.0...0.0..000...00..0.0...0.0..0...0..000..0.0.0000...0.00..00000..000.0.\n",
+      "00.00.0..0....000.....00000.0...0.0.......0.0.00..0..0.000..0.00000..000..0..000\n",
+      "....0.0.0.....0.......0.....0.....0...............0.0.0.....0.0.0...0.......0.0.\n"
+    ])
+  end
+
+  def test_it_can_split_into_more_letters
+    night_reader = NightReader.new("would_be_input", "would_be_output")
+    assert_equal " testing and then some", night_reader.convert_into_letters([
+      "...00..0.0.00000..0.0000...00.0.00...00.000.\n",
+      "..00.00.000..000.....0.0..0000.0.0..0..0...0\n",
+      "..0...0.0...0.......0.....0.....0...0.0.0...\n"
+    ])
+  end
+
+  def test_it_can_slice_braille
+    night_reader = NightReader.new("would_be_input", "would_be_output")
+    assert_equal ["00..", ".0..", ".000"], night_reader.sliced_braille(["0.00.."], ["0.00..", "0..0..", "00.000"])
+  end
+
+  def test_it_can_convert_braille
+    night_reader = NightReader.new("would_be_input", "would_be_output")
+    assert_equal "hey hey", night_reader.convert_into_letters(["0.00..0..0..00.000......0.00..0..0..00.000"])
+  end
 end
