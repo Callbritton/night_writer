1a35a432a5abc26b9debe47199fbed40fbc956e0
Chris Allbritton <cla.inbox@gmail.com>
2020-08-04 08:42:10 -0400

    Refactor to improve namability and update comments

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -8,16 +8,16 @@ class ConverterTest < Minitest::Test
 
   def test_it_can_convert_into_letters
     night_reader = NightReader.new("would_be_input", "would_be_output")
-    assert_equal "hello aric this is a big long string for", night_reader.convert_into_letters([
-      "0.0.0.0.0...0.0..000...00..0.0...0.0..0...0..000..0.0.0000...0.00..00000..000.0.\n",
-      "00.00.0..0....000.....00000.0...0.0.......0.0.00..0..0.000..0.00000..000..0..000\n",
-      "....0.0.0.....0.......0.....0.....0...............0.0.0.....0.0.0...0.......0.0.\n"
+    assert_equal "here is another really long string ", night_reader.convert_into_strings([
+      "0.0.0.0....0.0..0.000..00.0.0...0.0.0.0.0.00..0.0.0000...0.00..00000..\n",
+      "00.000.0..0.0......0.00000.000..00.0..0.0..0..0..0.000..0.00000..000..\n",
+      "....0.......0.....0.0.0.....0...0.....0.0.00..0.0.0.....0.0.0...0.....\n"
     ])
   end
 
   def test_it_can_split_into_more_letters
     night_reader = NightReader.new("would_be_input", "would_be_output")
-    assert_equal " testing and then some", night_reader.convert_into_letters([
+    assert_equal " testing and then some", night_reader.convert_into_strings([
       "...00..0.0.00000..0.0000...00.0.00...00.000.\n",
       "..00.00.000..000.....0.0..0000.0.0..0..0...0\n",
       "..0...0.0...0.......0.....0.....0...0.0.0...\n"
@@ -26,19 +26,24 @@ class ConverterTest < Minitest::Test
 
   def test_it_can_slice_braille
     night_reader = NightReader.new("would_be_input", "would_be_output")
-    assert_equal ["00..", ".0..", ".000"], night_reader.sliced_braille(["0.00.."], ["0.00..", "0..0..", "00.000"])
+    assert_equal ["00.."], night_reader.sliced_braille("", ["0.00.."])
   end
 
   def test_it_can_convert_braille
     night_reader = NightReader.new("would_be_input", "would_be_output")
-    assert_equal "hey hey", night_reader.convert_into_letters(["0.00..0..0..00.000......0.00..0..0..00.000"])
+    assert_equal "h", night_reader.convert_braille([["0.", "00", ".."]])
   end
 
-  def test_input_file_contents
-    File.expects(:readlines).with("message.txt").returns("Input beep boop")
-    night_reader = NightReader.new("message.txt", "braille.txt")
-    assert_equal "Input beep boop", night_reader.input_file_contents
+  def test_it_can_convert_braille_into_letters
+    night_reader = NightReader.new("would_be_input", "would_be_output")
+    assert_equal "hey hey", night_reader.convert_into_strings(["0.00..0..0..00.000......0.00..0..0..00.000"])
   end
+
+  # def test_input_file_contents
+  #   File.expects(:readlines).with("message.txt").returns("Input beep boop")
+  #   night_reader = NightReader.new("message.txt", "braille.txt")
+  #   assert_equal "Input beep boop", night_reader.input_file_contents
+  # end
 
   # def test_it_can_execute_conversion_to_letters
   #   night_reader = NightReader.new("would_be_input", "would_be_output")
